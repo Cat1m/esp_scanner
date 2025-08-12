ac1f06a421ab45bbab83004ed7e4f2ac78aa6a21
Lê Minh Chiến <122712259+Cat1m@users.noreply.github.com>
2025-08-12 16:54:06 +0700

    cập nhật ios

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import connectivity_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
 }
